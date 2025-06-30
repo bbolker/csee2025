1034f07befce45798df0abcc05311d4b359a981f
Ben Bolker <bbolker@gmail.com>
2025-06-30 14:49:04 -0400

    more on CIs

--- comp_ci.R
+++ comp_ci.R
@@ -1,8 +1,32 @@
 library(Matrix)
+library(scam)
+library(mgcv)
+library(MASS)
 ifun <- function(x, ...) {
   image(Matrix(x), ...)
 }
 
+
+set.seed(4)
+n <- 200
+x1 <- runif(n)*6-3
+f1 <- 3*exp(-x1^2) # unconstrained term
+y <- f1+rnorm(n)*.5
+dat <- data.frame(x1=x1,x2=x2,y=y)
+## fit model, get results, and plot...
+b <- scam(y~s(x2,bs="cv"),data=dat)
+summary(b)
+plot(b,pages=1,shade=TRUE)
+
+pp <- predict(b, se.fit=TRUE)
+with(pp,
+     matplot())
+
+
+
+## want to use Vp (no Vc available)
+## predict.scam uses: sqrt(rowSums((X %*% object$Vp.t) * X)) for se.fit
+## pick a scam example ...
 ## must be transformed variant ... back-transform?
 Sigma <- mgcv::vcov.gam(m_scam_tedecv)
 ifun(Sigma)
